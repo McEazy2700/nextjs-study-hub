dadc8dc538b3bb4d7e121ea770979775327661d6
Ezekiel Victor <codeepoch@gmail.com>
2022-11-16 19:56:00 +0100

    updated pdf download button

--- features/components/bank/links/Link.tsx
+++ features/components/bank/links/Link.tsx
@@ -14,3 +14,12 @@ export const PrimaryLink = (props: PrimaryLinkProps)=>{
     target='_blank' download={download} href={href}>{children}</Link>
   )
 }
+
+export const DownloadLink = (props: PrimaryLinkProps)=>{
+  const { href, children } = props
+  return (
+    <a
+    className="bg-primary/90 p-2 px-4 rounded-3xl absolute top-0 right-3 z-20"
+    target='_blank' download href={href}>{children}</a>
+  )
+}
--- features/components/resources/resource-pdf-reader/Reader.tsx
+++ features/components/resources/resource-pdf-reader/Reader.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PrimaryLink } from '@components/bank/links/Link'
+import { DownloadLink } from '@components/bank/links/Link'
 import { Viewer } from '@react-pdf-viewer/core'
 import { thumbnailPlugin } from '@react-pdf-viewer/thumbnail';
 import { CloseButton } from '@components/bank/buttons/CloseButton';
@@ -14,7 +14,7 @@ const Reader = (props: ReaderProps) => {
   return (
     <div className='p-4 min-w-[85vw] max-w-[90vw] overflow-y-scroll max-h-screen md:px-16 top-3 overflow-hidden rounded-xl flex items-center justify-center w-full relative'>
       <CloseButton onClick={close} className='fixed top-1 bg-red-600 text-secondary p-2 font-bold text-lg rounded-full left-3' />
-      <PrimaryLink href={href} download>Download</PrimaryLink>
+      <DownloadLink href={href} download>Download</DownloadLink>
       <Viewer plugins={[thumbnailPluginInstance]}  theme={{theme: 'auto'}} fileUrl={href} />
     </div>
   )
