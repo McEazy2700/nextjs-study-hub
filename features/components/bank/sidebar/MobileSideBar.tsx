cfffcd0b9eb6b8e58f778a41a72f2b96bb12f575
Ezekiel Victor <codeepoch@gmail.com>
2022-11-17 03:24:30 +0100

    updated mobile sidebar styles

--- features/components/bank/sidebar/MobileSideBar.tsx
+++ features/components/bank/sidebar/MobileSideBar.tsx
@@ -44,26 +44,26 @@ const MobileSideBar = ({ children, context }: BarType)=>{
   return (
     <motion.div
       ref={barRef}
-      animate={{ translateX: !barContext.isOpen ? '-102%' : 0  }}
+      animate={{ translateX: !barContext.isOpen ? '-0%' : 0  }}
       transition={{ duration: 0.2 }}
       className="min-h-[99vh] z-50 m-1 w-60 rounded-md fixed top-0 left-0 bg-dark-accent text-white">
       <div className="min-h-full p-3 relative min-w-full">
-        <button className="absolute -bottom-32 bg-dark-accent p-3 pl-6 rounded-r-full -right-10" type="button" onClick={expandHandler}>
-            {barContext.isOpen ? 
-            <motion.span
-              animate={{ opacity: 1 }}
-              ><FaAngleDoubleLeft /></motion.span>
-            : <motion.span
-              animate={{ opacity: 1 }}
-              ><FaAngleDoubleRight /></motion.span>
-            }
-          </button>
           {children}
       </div>
       <div className="absolute bottom-1 flex flex-col">
         <SideBarProfile context={context}/>
         <SideBarItem className="ml-2" href="/signout" text="Sign Out" icon={<FaSignOutAlt />} context={context}/>
       </div>
+      <button className="absolute bottom-1 bg-dark-accent p-3 pl-6 rounded-r-full -right-10" type="button" onClick={expandHandler}>
+          {barContext.isOpen ? 
+          <motion.span
+            animate={{ opacity: 1 }}
+            ><FaAngleDoubleLeft /></motion.span>
+          : <motion.span
+            animate={{ opacity: 1 }}
+            ><FaAngleDoubleRight /></motion.span>
+          }
+      </button>
     </motion.div>
   )
 }
